a9449ae7ae7da5c61ee8ba29d72bb3b62af9bab8
SamitMahadi <samithossainmahadi@gmail.com>
2022-10-11 08:08:36 -0400

    Home Done

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Header = () => {
     return (
-    <nav className='bg-slate-900 h-16	flex items-center justify-between pl-36 pr-36'>
-     <h2 className='text-white text-4xl font-bold'>Code-Skill</h2>
+    <nav className='bg-slate-900 border-b-2 border-pink-500 h-16	flex items-center justify-between pl-36 pr-36'>
+     <h2 className='text-white text-4xl font-bold'>Code-Skillz</h2>
         <div>
            <a className='text-white text-base ml-5 hover:text-pink-500' href="/">Home</a>
            <a className='text-white text-base ml-5 hover:text-pink-500' href="/statistics">Statistics</a>
--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import './Home.css'
+
 
 const Home = () => {
     return (
         <div>
+           
+              <h1 className=' pt-52 text-6xl text-center font-bold text-white underline decoration-wavy bg-scroll decoration-pink-500'>Welcome To Code-Skillz</h1>
             
         </div>
     );
