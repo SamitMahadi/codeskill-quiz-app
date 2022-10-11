a090942e1fc46ac3c1f50e64f3553ed2c1cc1815
SamitMahadi <samithossainmahadi@gmail.com>
2022-10-11 16:26:08 -0400

    blog done

--- src/Components/Blog/Blog.js
+++ src/Components/Blog/Blog.js
@@ -2,10 +2,18 @@ import React from 'react';
 
 const Blog = () => {
     return (
-        <div>
-          <h3>This is Blog</h3>  
+        <div classname='bg-slate-900 h-full'>
+          <h3 className='bg-slate-900   text-5xl text-center font-bold text-white underline decoration-wavy bg-scroll decoration-pink-500'>Welcome To Our Blog</h3>
+          <h4 className='text-center bg-slate-900 pt-7 text-white text-3xl'><span className='text-pink-500 text-3xl'>1.</span> What is the purpose of react router?</h4>
+          <p className='text-center  bg-slate-900  text-white text-2xl pt-4'>React Router is a standard library for routing in React. It enables the navigation among views of various components in a React Application, allows changing the browser URL, and keeps the UI in sync with the URL.</p>
+          <h4 className='text-center bg-slate-900 pt-7 text-white text-3xl'><span className='text-pink-500 text-3xl'>2.</span> How does Context-Api works?</h4>
+          <p className='text-center  bg-slate-900  text-white text-2xl pt-4'>The React Context API is a way for a React app to effectively produce global variables that can be passed around. This is the alternative to "prop drilling" or moving props from grandparent to child to parent, and so on. Context is also touted as an easier, lighter approach to state management using Redux.</p>
+          <h4 className='text-center bg-slate-900 pt-7 text-white text-3xl'><span className='text-pink-500 text-3xl'>2.</span> What is useRef hook?</h4>
+          <p className='text-center  bg-slate-900  text-white text-2xl pt-4'>          <p className='text-center  bg-slate-900  text-white text-2xl pt-4'>The React Context API is a way for a React app to effectively produce global variables that can be passed around. This is the alternative to "prop drilling" or moving props from grandparent to child to parent, and so on. Context is also touted as an easier, lighter approach to state management using Redux.</p>
+.</p>
+
         </div>
     );
 };
 
-export default Blog
+export default Blog;
